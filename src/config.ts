772e6dc16676d88f63aef0ac3d9c440e2e27c440
keduolim <1529668069@qq.com>
2025-09-01 17:15:22 +0800

    1

--- src/config.ts
+++ src/config.ts
@@ -224,7 +224,7 @@ export const announcementConfig: AnnouncementConfig = {
 	content: "欢迎来到我的博客！这里致力于分享网安知识。", // 公告内容
 	closable: false, // 允许用户关闭公告
 	link: {
-		enable: true, // 启用链接
+		enable: false, // 启用链接
 		text: "了解更多", // 链接文本
 		url: "/about/", // 链接 URL
 		external: false, // 内部链接
